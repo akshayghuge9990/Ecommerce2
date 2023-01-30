6b26d27fada9437b46dd7076094553205acfdc62
akshayghuge9990 <ghugeakshay123@gmail.com>
2023-01-30 16:29:02 +0530

    Changes by AkshayG

---
+++ src/main/java/com/akshay/EcommerceProject/EcommerceProject2Application.java
@@ -0,0 +1,13 @@
+package com.akshay.EcommerceProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceProject2Application.class, args);
+	}
+
+}
